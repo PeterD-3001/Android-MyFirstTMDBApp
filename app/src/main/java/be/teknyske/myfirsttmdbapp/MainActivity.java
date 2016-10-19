3941794a80cc3fa8e6ac97ae361f4bc729c7af34
Peter Derous <peter.derous@skynet.be>
2016-10-19 15:55:01 +0200

    Initial commit

---
+++ app/src/main/java/be/teknyske/myfirsttmdbapp/MainActivity.java
@@ -0,0 +1,164 @@
+package be.teknyske.myfirsttmdbapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.omertron.themoviedbapi.MovieDbException;
+import com.omertron.themoviedbapi.TheMovieDbApi;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+{
+    private TheMovieDbApi api;
+    final static String API_KEY = "4d47b8fb2f4125a5bd76ab0f464a6b5f";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+        {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+                {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        DisplayImageOptions defaultOptions =
+                new DisplayImageOptions.Builder()
+                        .cacheInMemory(true)
+                        .cacheOnDisk(true)
+                        .showImageForEmptyUri(R.drawable.ic_wait)
+                        .showImageOnLoading(R.drawable.ic_wait)
+                        .displayer(new FadeInBitmapDisplayer(500))
+                        .build();
+
+        ImageLoaderConfiguration config =
+                new ImageLoaderConfiguration.Builder(getApplicationContext())
+                        .defaultDisplayImageOptions(defaultOptions)
+                        .denyCacheImageMultipleSizesInMemory()
+                        .build();
+
+        ImageLoader.getInstance().init(config);
+
+        try
+            {
+                api = new TheMovieDbApi(API_KEY);
+                FetchConfiguration fetchConfiguration = new FetchConfiguration();
+                // AsyncTask 1
+                fetchConfiguration.execute();
+
+                FetchMovieInfo fetchMovieInfo = new FetchMovieInfo();
+                // AsyncTask 2
+                fetchMovieInfo.execute();
+            }
+        catch (MovieDbException e)
+            {
+                e.printStackTrace();
+                Log.e("TheMovieDBAPI", "Error: " + e.getMessage());
+            }
+
+
+        }
+
+    @Override
+    public void onBackPressed()
+        {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            {
+            drawer.closeDrawer(GravityCompat.START);
+            } else
+            {
+            super.onBackPressed();
+            }
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+        {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+        {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+            {
+            return true;
+            }
+
+        return super.onOptionsItemSelected(item);
+        }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+        {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera)
+            {
+            // Handle the camera action
+            } else if (id == R.id.nav_gallery)
+            {
+
+            } else if (id == R.id.nav_slideshow)
+            {
+
+            } else if (id == R.id.nav_manage)
+            {
+
+            } else if (id == R.id.nav_share)
+            {
+
+            } else if (id == R.id.nav_send)
+            {
+
+            }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+        }
+}
